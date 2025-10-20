c2cd887328df5fc07858a2bd5d14445e6ff33c27
Andy <gabryelb027@gmail.com>
2025-10-19 21:06:15 -0500

    Primer commit

---
+++ src/app/pages/laboratory-component/laboratory-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-laboratory-component',
+  imports: [],
+  templateUrl: './laboratory-component.html',
+  styleUrl: './laboratory-component.css'
+})
+export class LaboratoryComponent {
+
+}

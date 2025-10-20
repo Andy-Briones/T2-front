f448c33764c6954cf7a5c0eeaa89b2b39044f5dc
Andy <gabryelb027@gmail.com>
2025-10-19 21:22:45 -0500

    creacion de componentes, modelos, servicios y el environment

---
+++ src/app/pages/family-component/family-edit-component/family-edit-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-family-edit-component',
+  imports: [],
+  templateUrl: './family-edit-component.html',
+  styleUrl: './family-edit-component.css'
+})
+export class FamilyEditComponent {
+
+}
---
+++ src/app/pages/laboratory-component/laboratory-edit-component/laboratory-edit-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-laboratory-edit-component',
+  imports: [],
+  templateUrl: './laboratory-edit-component.html',
+  styleUrl: './laboratory-edit-component.css'
+})
+export class LaboratoryEditComponent {
+
+}
